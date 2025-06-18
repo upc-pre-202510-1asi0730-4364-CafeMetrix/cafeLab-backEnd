99cdcebaac34362b0d0d8374630049a1780b2e01
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2025-06-18 17:42:57 -0500

    feat(infrastructure): add profile-repository

---
+++ CafeLab.API/Profiles/Infrastructure/Persistance/EFC/Repositories/ProfileRepository.cs
@@ -0,0 +1,23 @@
+using CafeLab.API.Profiles.Domain.Model.Aggregates;
+using CafeLab.API.Profiles.Domain.Model.ValueObjects;
+using CafeLab.API.Profiles.Domain.Repositories;
+using CafeLab.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using CafeLab.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace CafeLab.API.Profiles.Infrastructure.Persistance.EFC.Repositories;
+
+/// <summary>
+/// Profile repository implementation  
+/// </summary>
+/// <param name="context">
+/// The database context
+/// </param>
+public class ProfileRepository(AppDbContext context) 
+    : BaseRepository<Profile>(context), IProfileRepository
+{
+    /// <inheritdoc />
+    public async Task<Profile?> FindProfileByEmailAsync(EmailAddress email)
+    {
+        return Context.Set<Profile>().FirstOrDefault(p => p.Email == email);
+    }
+}
